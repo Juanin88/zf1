6a7189a2ff2063e076a132fe404b49b989263e40
juan <ing.juan.garfias@gmail.com>
2017-11-03 01:44:05 -0600

    test

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -23,7 +23,7 @@ class TestController extends Zend_Controller_Action
     	
     	$user = 'ee17b0fb-8138-4401-8e52-4876101b1faf';
     	$password = 'g7bi=b9bZiPu';
-    	
+    	die('test');
     	$srPago = new Application_Model_Services_SrPago();
     
     	$this->_helper->json( $srPago->getSrPagoOperations( $srPago->getToken($user, $password) ) );
