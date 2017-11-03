b22ce88cfc72e2640e93daf94ebf0aa19fb0a379
juan <ing.juan.garfias@gmail.com>
2017-11-03 01:15:26 -0600

    test

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -31,39 +31,39 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		return $view;
 	}
 	
-// 	protected function _initAutoload() {
-// 		$moduleLoader = new Zend_Application_Module_Autoloader (
-// 				array ('namespace' => '',
-// 						'basePath' => APPLICATION_PATH
-// 				)
-// 				);
+	protected function _initAutoload() {
+		$moduleLoader = new Zend_Application_Module_Autoloader (
+				array ('namespace' => '',
+						'basePath' => APPLICATION_PATH
+				)
+				);
 	
-// 		$this->bootstrap('FrontController');
-// 		$front =  $this->getResource('FrontController');
-// 		$front->registerPlugin(new Plugin_Acl() );
+		$this->bootstrap('FrontController');
+		$front =  $this->getResource('FrontController');
+		//$front->registerPlugin(new Plugin_Acl() );
 	
-// 		return $moduleLoader;
-// 	}
+		return $moduleLoader;
+	}
 	
 	/**
 	 * Initializing session
 	 */
 	protected function _initSession() {
-		//Zend_Session::start();
+		Zend_Session::start();
 	
 		// Instance namespace Zend_Auth
-// 		$authNamespace = new Zend_Session_Namespace('Zend_Auth');
-// 		$authNamespace->forcePasswordChange = false;
-// 		// clear the identity of a user who has not accessed a controller for
-// 		// longer than a timeout period.
-// 		if(isset($authNamespace->timeout) && time() > $authNamespace->timeout){
-// 			$storage = new Zend_Auth_Storage_Session();
-// 			$storage->clear();
-// 			$this->bootstrap('view');
-// 			$this->view = $this->getResource('view');
-// 			$this->view->errorMessageTimeout = "<strong>" . $this->view->translate('Alert') .":</strong>" . $this->view->translate('Su Sesi&oacute;n ha caducado por inactividad en el sistema');
+		$authNamespace = new Zend_Session_Namespace('Zend_Auth');
+		$authNamespace->forcePasswordChange = false;
+		// clear the identity of a user who has not accessed a controller for
+		// longer than a timeout period.
+		if(isset($authNamespace->timeout) && time() > $authNamespace->timeout){
+			$storage = new Zend_Auth_Storage_Session();
+			$storage->clear();
+			$this->bootstrap('view');
+			$this->view = $this->getResource('view');
+			$this->view->errorMessageTimeout = "<strong>" . $this->view->translate('Alert') .":</strong>" . $this->view->translate('Su Sesi&oacute;n ha caducado por inactividad en el sistema');
 	
-// 		}
+		}
 	}
 }
 
